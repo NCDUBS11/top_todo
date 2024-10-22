45470b709c6971d5e25868402f6a9bc25cb81fee
nick <112791837+NCDUBS11@users.noreply.github.com>
2024-10-22 06:27:56 -0700

    Add new project menu, no JS functionality yet.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import "./styles.css";
 import newTask from "./new-task";
+import newProject from "./new-project";
 import { compareAsc, format, formatDistance, formatDistanceToNow } from "date-fns";
 //import { function name } from "./jsFile";
 //import odinImage from "./odin.png";
@@ -8,8 +9,14 @@ if (process.env.NODE_ENV !== "production") {
     console.log("Looks like we are in development mode!");
 };
 
-const newTaskBtn = document.getElementById("newTask")
+const newTaskBtn = document.getElementById("newTask");
+const newProjectBtn = document.getElementById("newProject");
+
 
 newTaskBtn.addEventListener("click", ()=>{
     newTask();
+});
+
+newProjectBtn.addEventListener("click", ()=>{
+    newProject();
 });
---
+++ src/new-project.js
@@ -0,0 +1,44 @@
+export default function newProject(){
+
+    const mainArea = document.getElementById("main");
+
+        mainArea.innerHTML = `
+        <div id="newProjectMenu" class="newProjectMenu">
+            <form action="" id="form" class="form">
+                <fieldset>
+                    <legend>New Project</legend>
+                    <div class="formControl projectName">
+                        <label for="projectName">Project Title:</label>
+                        <input type="text" name="projectName" id="projectName" class="projectName"
+                        placeholder="Project Title">
+                            <img id="error" class="error" src="icons/xmark-svgrepo-com.png" alt="">
+                    </div>
+                    <div class="formControl projectDescription">
+                        <label for="projectDescription">Description:</label>
+                        <textarea type="text" rows="5" name="projectDescription" id="projectDescription" class="taskDescription"
+                        placeholder="Project Description"></textarea>
+                        <img id="error" class="error" src="icons/xmark-svgrepo-com.png" alt="">
+                    </div>
+                    <div class="formControl submitNewProject">
+                        <button id="submitBtn" class="submitBtn" type="button" onclick="">Submit</button>
+                        <button id="cancelBtn" class="cancelBtn" type="button" onclick="">Cancel</button>
+                        <p id="error" class="error">'error desc'</p>
+                    </div>
+                </fieldset>
+            </form>
+        </div>
+        `;
+
+        const newProjectForm = document.getElementById("form");
+        const submitBtn = document.getElementById("submitBtn");
+        const cancelBtn = document.getElementById("cancelBtn");
+        const projectName = document.getElementById("projectName");
+        const projectDescription = document.getElementById("projectDescription");
+
+        cancelBtn.addEventListener("click", ()=>{
+            newProjectForm.reset();
+            mainArea.innerHTML = "";
+        })
+
+
+};
